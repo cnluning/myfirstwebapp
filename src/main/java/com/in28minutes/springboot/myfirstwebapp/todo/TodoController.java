64cd035a9563cd122d83bcacaab97c340fcd3102
Claire Luning <cnluning@gmail.com>
2022-09-12 12:23:49 -0500

    update course progress

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -1,10 +1,13 @@
 package com.in28minutes.springboot.myfirstwebapp.todo;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
@@ -25,6 +28,21 @@ public class TodoController {
 		model.addAttribute("todos", todos);
 		
 		return "listTodos";
+	}
+	
+	@RequestMapping(value="add-todo", method = RequestMethod.GET)
+	public String showNewTodoPage(ModelMap model) {
+		String username = (String)model.get("name");
+		Todo todo = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+		model.put("todo", todo);
+		return "todo";
+	}
+	
+	@RequestMapping(value="add-todo", method = RequestMethod.POST)
+	public String addNewTodo(ModelMap model, Todo todo) {
+		String username = (String)model.get("name");
+		todoService.addTodo(username, todo.getDescription(), LocalDate.now().plusYears(1), false);
+		return "redirect:list-todos";
 	}
 
 }
