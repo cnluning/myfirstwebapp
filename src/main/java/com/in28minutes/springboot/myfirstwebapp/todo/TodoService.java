8d0283632bb4f2ba8b1ab9540833988e1a240c00
Claire Luning <cnluning@gmail.com>
2022-09-14 12:30:08 -0500

    update course progress

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 import org.springframework.stereotype.Service;
 
@@ -30,4 +31,13 @@ public class TodoService {
 		Todo todo = new Todo(++todosCount, username, description, targetDate, done);
 		todos.add(todo);
 	}
+	
+	public void deleteById(int id) {
+		//todo.getId() == id
+		//todo -> todo.getId() == id
+		Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+		todos.removeIf(predicate);
+	}
 }
+
+
