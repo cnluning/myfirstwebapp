9a17747eacb77a20f81a3282b260a9a6047b5ce8
Claire Luning <cnluning@gmail.com>
2022-11-02 14:42:52 -0500

    create TodoRepository and connect list todos page to h2 db

--- src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoControllerJpa.java
+++ src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoControllerJpa.java
@@ -15,22 +15,26 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 
 import jakarta.validation.Valid;
 
-//@Controller
+@Controller
 @SessionAttributes("name")
-public class TodoController {
+public class TodoControllerJpa {
 	
-	public TodoController(TodoService todoService) {
+	public TodoControllerJpa(TodoService todoService, TodoRepository todoRepository) {
 		super();
 		this.todoService = todoService;
+		this.todoRepository = todoRepository;
 	}
 
 	private TodoService todoService;
-		
+	
+	private TodoRepository todoRepository; 
+	
 	
 	@RequestMapping("list-todos")
 	public String listAllTodos(ModelMap model) {
 		String username = getLoggedInUsername(model);
-		List<Todo> todos = todoService.findByUsername(username);
+		
+		List<Todo> todos = todoRepository.findByUsername(username);
 		model.addAttribute("todos", todos);
 		
 		return "listTodos";
